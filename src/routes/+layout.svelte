d2884385046d030f0b2668e3045fa32cc9f92ef6
taoki <84th.tccaoki@gmail.com>
2026-08-19 17:20:54 +0900

    feat(layout): ヘッダーの基本的なuiを追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,22 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<header class="fixed top-4 left-1/2 z-20 w-[90%] -translate-x-1/2 rounded-4xl border border-black/10 bg-white/80 backdrop-blur-md transition-all duration-500 md:w-[80%] lg:w-[60%]">
+	<div class="flex">
+		<a href="/">
+			<p>tohofes.jp</p>
+		</a>
+		<nav class="ml-auto flex list-none items-center gap-5 whitespace-nowrap transition">
+			<li><a href="/" class="ml-1 text-sm tracking-wider transition">Home</a></li>
+			<li>
+				<a href="/about" class="ml-3 text-sm tracking-wider transition">About</a>
+			</li>
+		</nav>
+	</div>
+</header>
+
 {@render children()}
