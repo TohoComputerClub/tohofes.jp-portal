ebb5d1ea4ece9133830449733c8330e0fb891da2
taoki <84th.tccaoki@gmail.com>
2026-08-19 21:10:56 +0900

    feat(page): トップセクションを追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section class="container mx-auto flex flex-col justify-center min-h-screen">
+    <div class="flex flex-col mx-4 md:mx-20">
+        <h2 class="mx-auto text-3xl">Comming Soon ...</h2>
+        <br />
+        <h2 class="text-xl mb-2 mx-auto">過去の公式ホームページ</h2>
+        <div class="flex mx-auto">
+            <a href="https://75th.tohofes.jp/" class="">
+                <i class="fa-solid fa-arrow-up-right-from-square mr-1 text-sm"></i>第75回
+            </a>
+        </div>
+    </div>
+</section>
