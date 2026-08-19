ef5a09bfc4771baa7f2e28e407337e092f79d219
taoki <84th.tccaoki@gmail.com>
2026-08-19 18:00:15 +0900

    feat(error): エラーページのデザインを更新

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,16 +4,20 @@
 
 </script>
 
-<h1>404 - ページが見つかりません</h1>
-
-{#if $page.status === 404}
-	<p>お探しのページは見つかりませんでした。</p>
-	<p>以下のリンクをご確認ください：</p>
-
-	<!-- 生成された動的リンク -->
-	<a href='https://75th.tohofes.jp{currentPath}'>
-		75th.tohofes.jp{currentPath}
-	</a>
-{:else}
-	<p>エラーが発生しました: {$page.status}</p>
-{/if}
+<section class="container mx-auto mt-20">
+    <div class="flex flex-col mx-4 md:mx-20">
+        <h1 class="text-8xl mx-auto mt-4">404</h1>
+        <h1 class="text-5xl mx-auto mt-2">Not Found</h1>
+        <h2 class="text-3xl mx-auto mt-4">ページが見つかりません</h2>
+        <hr class="my-10"/>
+        {#if $page.status === 404}
+            <p>お探しのページは見つかりませんでした。</p>
+            <p>過去の桐朋祭ホームページにアクセスしたい場合は以下のリンクをご確認ください。</p>
+            <a href='https://75th.tohofes.jp{currentPath}'>
+                https://75th.tohofes.jp{currentPath}
+            </a>
+        {:else}
+            <p>エラーが発生しました: {$page.status}</p>
+        {/if}
+    </div>
+</section>
