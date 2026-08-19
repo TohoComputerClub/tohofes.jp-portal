c6b5248d99beac95da67234cd378e762e0d39c15
taoki <84th.tccaoki@gmail.com>
2026-08-19 21:41:40 +0900

    del(page): 当サイトについてセクションを削除

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,15 +13,6 @@
 <hr />
 <section class="container mx-auto flex flex-col">
     <div class="flex flex-col mx-4 my-4 md:mx-20">
-        <h2 class="text-2xl" id="about">当サイトについて</h2>
-        <p class="mt-4">
-            サイトの説明
-        </p>
-    </div>
-</section>
-<hr />
-<section class="container mx-auto flex flex-col">
-    <div class="flex flex-col mx-4 my-4 md:mx-20">
         <h2 class="text-2xl" id="about">SNS / リンク</h2>
         <div class="grid grid-cols-1 gap-4 my-4 md:grid-cols-2 lg:grid-cols-3">
             <a
