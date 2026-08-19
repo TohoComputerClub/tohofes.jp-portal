bb7117e4dc67f027ae6c4d0e4eab3fde021f76f3
taoki <84th.tccaoki@gmail.com>
2026-08-19 18:05:55 +0900

    feat(error): エラーページ内のテキストのフォントサイズの変更

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -11,13 +11,17 @@
         <h2 class="text-3xl mx-auto mt-4">ページが見つかりません</h2>
         <hr class="my-10"/>
         {#if $page.status === 404}
-            <p>お探しのページは見つかりませんでした。</p>
-            <p>過去の桐朋祭ホームページにアクセスしたい場合は以下のリンクをご確認ください。</p>
-            <a href='https://75th.tohofes.jp{currentPath}'>
-                https://75th.tohofes.jp{currentPath}
+            <p class="text-lg mb-4">お探しのページは見つかりませんでした。</p>
+            <p class="text-lg">過去の桐朋祭ホームページにアクセスしたい場合は以下のリンクをご確認ください。</p>
+            <a href='https://75th.tohofes.jp{currentPath}' class="text-lg w-fit transition-all hover:underline">
+                <i class="fa-solid fa-arrow-up-right-from-square mr-1 text-sm"></i>https://75th.tohofes.jp{currentPath}
             </a>
         {:else}
             <p>エラーが発生しました: {$page.status}</p>
         {/if}
+        <hr class="my-10"/>
+        <a href='/' class="text-lg w-fit transition-all hover:underline">
+            <i class="fa-solid fa-house text-sm"></i>トップへ戻る
+        </a>
     </div>
 </section>
