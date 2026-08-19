549c6271c20645ed1d2e47bb4cedf13241022c15
taoki <84th.tccaoki@gmail.com>
2026-08-19 17:10:02 +0900

    add(error): アーカイブへのリダイレクト機能付きエラーページを追加

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from '$app/stores';
+	let currentPath = $derived($page.url.pathname)
+
+</script>
+
+<h1>404 - ページが見つかりません</h1>
+
+{#if $page.status === 404}
+	<p>お探しのページは見つかりませんでした。</p>
+	<p>以下のリンクをご確認ください：</p>
+
+	<!-- 生成された動的リンク -->
+	<a href='https://75th.tohofes.jp{currentPath}'>
+		75th.tohofes.jp{currentPath}
+	</a>
+{:else}
+	<p>エラーが発生しました: {$page.status}</p>
+{/if}
