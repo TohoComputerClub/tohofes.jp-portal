fde117bad2147f72f18e5a513aabf24776fdbfa3
taoki <84th.tccaoki@gmail.com>
2026-08-20 20:30:38 +0900

    feat(page): ヘッダーテキストのマージンを変更

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 <header class="fixed items-center top-4 left-1/2 z-20 w-[90%] -translate-x-1/2 rounded-4xl border border-black/10 bg-white/80 backdrop-blur-md transition-all duration-500 md:w-[80%] lg:w-[60%]">
 	<div class="flex p-2">
 		<a href="/" class="flex transition h-12">
-			<p class="m-auto ml-2 block">桐朋祭</p>
+			<p class="m-auto ml-4 block">桐朋祭</p>
 		</a>
 		<nav class="ml-auto flex list-none items-center gap-5 whitespace-nowrap transition">
 			<li><a href="/" class="ml-1 text-sm tracking-wider transition">Home</a></li>
