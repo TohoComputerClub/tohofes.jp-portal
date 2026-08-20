655b97e81b2e0af972d75cfe82fef11ffab2d210
taoki <84th.tccaoki@gmail.com>
2026-08-20 20:29:21 +0900

    feat(page): トップセクションの過去HPリンク部分のデザイン変更とtarget_blankの追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,9 @@
     <div class="flex flex-col mx-4 md:mx-20">
         <h2 class="mx-auto text-3xl">Comming Soon ...</h2>
         <br />
-        <h2 class="text-xl mb-2 mx-auto">過去の公式ホームページ</h2>
+        <h2 class="text-xl mb-2 mx-auto"><i class="fa-solid fa-folder text-lg mr-1"></i>過去の公式ホームページ</h2>
         <div class="flex mx-auto">
-            <a href="https://75th.tohofes.jp/" class="">
+            <a href="https://75th.tohofes.jp/" class="hover:underline" target="_blank">
                 <i class="fa-solid fa-arrow-up-right-from-square mr-1 text-sm"></i>第75回
             </a>
         </div>
